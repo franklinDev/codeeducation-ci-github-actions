366a47a4d4e0d4cf5e5e99ee7e12dbe7a6178217
Franklin Andrade <franklin.dev7@gmail.com>
2023-01-16 22:07:56 -0300

    Testing fails.

--- test/sum.test.js
+++ test/sum.test.js
@@ -3,6 +3,6 @@ const assert = require('assert');
 
 describe('test sum', function () {
   it('should return 3 when sum 1 + 2', function () {
-    assert.equal(sum(1, 2), 3);
+    assert.equal(sum(1, 2), 4);
   });
 });
