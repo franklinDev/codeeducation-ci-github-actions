96b6e9a738f4219cae6ab8db76cc9d93f4e1eb04
Franklin Andrade <franklin.dev7@gmail.com>
2023-01-16 21:58:31 -0300

    First commit

---
+++ test/sum.test.js
@@ -0,0 +1,8 @@
+const sum = require('../sum');
+const assert = require('assert');
+
+describe('test sum', function () {
+  it('should return 3 when sum 1 + 2', function () {
+    assert.equal(sum(1, 2), 3);
+  });
+});
