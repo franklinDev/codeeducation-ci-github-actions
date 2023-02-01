4a151d1e5e73c144cba79ea7acb5c259f2cc0a50
Franklin Andrade <franklin.dev7@gmail.com>
2023-01-31 22:08:45 -0300

    Remove node modules.

---
+++ test/sub.test.js
@@ -0,0 +1,5 @@
+const sum = require('../sub');
+
+test("should return 3 when sub 6 - 3", () => {
+  expect(sub(6,3)).toBe(3)
+})
--- test/sum.test.js
+++ test/sum.test.js
@@ -1,5 +1,4 @@
 const sum = require('../sum');
-const assert = require('assert');
 
 test("should return 3 when sum 1 + 2", () => {
   expect(sum(1,2)).toBe(3)
