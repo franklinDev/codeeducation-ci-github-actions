87c9521571816261c1891822e1217c521d02be52
Franklin Andrade <franklin.dev7@gmail.com>
2023-02-01 00:07:57 -0300

    test

--- sum.js
+++ sum.js
@@ -5,19 +5,7 @@ function sum(a, b) {
 function sub(a, b) {
   return a - b;
 }
-function sub1(a, b) {
-  return a - b;
-}
-function sub2(a, b) {
-  return a - b;
-}
-function sub3(a, b) {
-  return a - b;
-}
 module.exports = {
   sum,
   sub,
-  sub1,
-  sub2,
-  sub3,
 };
